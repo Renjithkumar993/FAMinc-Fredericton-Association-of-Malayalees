42f9f4e5f97954533895fb2b73ad27a79cee4b9b
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2024-08-17 10:41:20 -0300

    gh

--- src/components/UsefulLinks.js
+++ src/components/UsefulLinks.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
-import { Grid, Card, CardContent, Typography, CardActionArea, Box, IconButton, Collapse } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Container, Grid, Card, CardContent, Typography, CardActionArea, Box, IconButton, Collapse, List, ListItem, ListItemText } from '@mui/material';
 import { ExpandMore, LocationCity, Work, AccountBalance, LocalHospital, ChildCare, Groups, CreditCard, Apartment } from '@mui/icons-material';
 import { Zoom } from 'react-awesome-reveal';
 
+const iconMapping = {
+  LocationCity: <LocationCity />,
+  Work: <Work />,
+  AccountBalance: <AccountBalance />,
+  LocalHospital: <LocalHospital />,
+  ChildCare: <ChildCare />,
+  Groups: <Groups />,
+  CreditCard: <CreditCard />,
+  Apartment: <Apartment />
+};
+
 const UsefulLinks = () => {
+  const [data, setData] = useState(null);
   const [expandedHealth, setExpandedHealth] = useState(false);
   const [expandedEmployment, setExpandedEmployment] = useState(false);
+  const [expandedImmigrant, setExpandedImmigrant] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${process.env.PUBLIC_URL}/config/usefullinks.json`);
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
 
   const handleExpandClickHealth = () => {
     setExpandedHealth(!expandedHealth);
@@ -15,38 +38,22 @@ const UsefulLinks = () => {
     setExpandedEmployment(!expandedEmployment);
   };
 
-  const healthLinks = [
-    { title: 'NB Medicare', description: 'Apply for your Medicare card.', link: 'https://www2.gnb.ca/content/gnb/en/departments/health/Medicare.html' },
-    { title: 'Fredericton Downtown Community Health Center', description: 'Request health services at the Downtown Community Health Center.', link: 'https://nb.skipthewaitingroom.com/walk-in-clinic/fredericton/horizon-clinics/fredericton-downtown-community-health-center-request-for-service' },
-    { title: 'EvisitNB with Maple', description: 'Virtual healthcare visits through EvisitNB.', link: 'https://www.getmaple.ca/evisitnb/' },
-    { title: 'Walk-in Clinic Locations', description: 'Walk-in clinics provide care for New Brunswick patients with non-urgent medical problems.', link: 'https://www.nbms.nb.ca/walk-in-clinics/' },
-  ];
+  const handleExpandClickImmigrant = () => {
+    setExpandedImmigrant(!expandedImmigrant);
+  };
 
-  const employmentLinks = [
-    { title: 'Government of Canada Jobs', link: 'https://emploisfp-psjobs.cfp-psc.gc.ca/psrs-srfp/applicant/page2440?fromMenu=true&toggleLanguage=en' },
-    { title: 'GC Digital Talent', link: 'https://talent.canada.ca/en/' },
-    { title: 'NB Jobs', link: 'https://www.nbjobs.ca/' },
-    { title: 'GNB Jobs', link: 'https://www2.gnb.ca/content/gnb/en/gateways/employment.html' },
-    { title: 'Indeed Jobs', link: 'https://ca.indeed.com/jobs?q=&l=Fredericton%2C+NB&from=searchOnDesktopSerp&vjk=0f814476e27b78fd' },
-    { title: 'Monster Jobs', link: 'https://www.monster.ca/jobs/search?q=&where=fredericton&page=1&so=p.s.sh' },
-    { title: 'CareerBeacon', link: 'https://www.careerbeacon.com/en/search/jobs-in-Fredericton_New-Brunswick' },
-  ];
-
-  const links = [
-    { title: 'New Brunswick Immigration Program Streams', description: 'Learn about the NBPNP process.', icon: <LocationCity />, link: 'https://www2.gnb.ca/content/gnb/en/corporate/promo/immigration/immigrating-to-nb/nb-immigration-program-streams.html' },
-    { title: 'Tax Filing', description: 'Personal income tax filing guidelines.', icon: <AccountBalance />, link: 'https://www.canada.ca/en/services/taxes/income-tax/personal-income-tax.html' },
-    { title: 'Child Care', description: 'Learn more about ChildCare in Fredericton.', icon: <ChildCare />, link: 'https://www.nbed.nb.ca/parentportal/en/search/elc/#lat=46.52936210750518,long=-66.17836761,zoom=7,opt=di:;a2:0;a25:0;as:0;oa:0;d:0;tc:0;th:0;le:0;lf:0;lb:0;po:0;on:||' },
-    { title: 'For New Immigrants', description: 'For newcomers to Canada.', icon: <Groups />, link: 'https://www.canada.ca/en/immigration-refugees-citizenship/services/new-immigrants.html' },
-    { title: 'Social Insurance Number', description: 'Submit your SIN application.', icon: <CreditCard />, link: 'https://www.canada.ca/en/employment-social-development/services/sin/apply.html' },
-    { title: 'Social Development Canada', description: 'Improve the standard of living.', icon: <Apartment />, link: 'https://www.canada.ca/en/employment-social-development.html' },
-  ];
+  if (!data) {
+    return <Typography>Loading...</Typography>;
+  }
 
   return (
-    <Box sx={{ padding: 2, marginTop: '80px', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', overflowX: 'hidden' }}>
+    <Container sx={{ marginTop: '80px', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
       <Typography variant="h4" gutterBottom align="center" sx={{ marginBottom: 4, color: '#ff6341' }}>
         Useful Links for Newcomers to New Brunswick
       </Typography>
       <Grid container spacing={4}>
+        
+        {/* Health Services Section */}
         <Grid item xs={12}>
           <Zoom triggerOnce={false}>
             <Card
@@ -61,8 +68,8 @@ const UsefulLinks = () => {
                 borderRadius: '15px',
                 '&:hover': {
                   backgroundColor: '#112240',
-                  transform: 'scale(1.05)',
-                  transition: '0.3s',
+                  transform: 'translateY(-10px)',
+                  transition: '0.3s ease-in-out',
                 },
               }}
             >
@@ -91,7 +98,7 @@ const UsefulLinks = () => {
                   >
                     Health Services
                   </Typography>
-                  <Typography variant="body2" sx={{ marginBottom: 2 }}>
+                  <Typography variant="body2" sx={{ marginBottom: 2, color: '#fff' }}>
                     Click to explore health-related services in New Brunswick.
                   </Typography>
                   <IconButton
@@ -108,19 +115,25 @@ const UsefulLinks = () => {
               </CardActionArea>
               <Collapse in={expandedHealth} timeout="auto" unmountOnExit>
                 <CardContent>
-                  {healthLinks.map((link, index) => (
-                    <Typography key={index} variant="body2" sx={{ marginBottom: 1 }}>
-                      <a href={link.link} target="_blank" rel="noopener noreferrer" style={{ color: '#ff6341', textDecoration: 'none' }}>
-                        {link.title} - {link.description}
-                      </a>
-                    </Typography>
-                  ))}
+                  <List>
+                    {data.healthLinks.map((link, index) => (
+                      <ListItem key={index}>
+                        <ListItemText
+                          primary={`${index + 1}. ${link.title}`}
+                          secondary={link.description}
+                          primaryTypographyProps={{ style: { color: '#ff6341' } }}
+                          secondaryTypographyProps={{ style: { color: '#fff' } }}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
                 </CardContent>
               </Collapse>
             </Card>
           </Zoom>
         </Grid>
 
+        {/* Employment Services Section */}
         <Grid item xs={12} sm={6} md={4}>
           <Zoom triggerOnce={false}>
             <Card
@@ -135,8 +148,8 @@ const UsefulLinks = () => {
                 borderRadius: '15px',
                 '&:hover': {
                   backgroundColor: '#112240',
-                  transform: 'scale(1.05)',
-                  transition: '0.3s',
+                  transform: 'translateY(-10px)',
+                  transition: '0.3s ease-in-out',
                 },
               }}
             >
@@ -165,7 +178,7 @@ const UsefulLinks = () => {
                   >
                     Employment Services
                   </Typography>
-                  <Typography variant="body2" sx={{ marginBottom: 2 }}>
+                  <Typography variant="body2" sx={{ marginBottom: 2, color: '#fff' }}>
                     Explore job opportunities.
                   </Typography>
                   <IconButton
@@ -182,20 +195,106 @@ const UsefulLinks = () => {
               </CardActionArea>
               <Collapse in={expandedEmployment} timeout="auto" unmountOnExit>
                 <CardContent>
-                  {employmentLinks.map((jobLink, index) => (
-                    <Typography key={index} variant="body2" sx={{ marginBottom: 1 }}>
-                      <a href={jobLink.link} target="_blank" rel="noopener noreferrer" style={{ color: '#ff6341', textDecoration: 'none' }}>
-                        {jobLink.title}
-                      </a>
-                    </Typography>
-                  ))}
+                  <List>
+                    {data.employmentLinks.map((jobLink, index) => (
+                      <ListItem key={index}>
+                        <ListItemText
+                          primary={`${index + 1}. ${jobLink.title}`}
+                          secondary={jobLink.description}
+                          primaryTypographyProps={{ style: { color: '#ff6341' } }}
+                          secondaryTypographyProps={{ style: { color: '#fff' } }}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
                 </CardContent>
               </Collapse>
             </Card>
           </Zoom>
         </Grid>
 
-        {links.map((link, index) => (
+        {/* For New Immigrants Section */}
+        <Grid item xs={12} sm={6} md={4}>
+          <Zoom triggerOnce={false}>
+            <Card
+              sx={{
+                backgroundColor: '#0c1a2c',
+                color: '#fff',
+                minHeight: '250px',
+                display: 'flex',
+                flexDirection: 'column',
+                padding: 3,
+                boxShadow: '0 8px 24px rgba(0, 0, 0, 0.3)',
+                borderRadius: '15px',
+                '&:hover': {
+                  backgroundColor: '#112240',
+                  transform: 'translateY(-10px)',
+                  transition: '0.3s ease-in-out',
+                },
+              }}
+            >
+              <CardActionArea onClick={handleExpandClickImmigrant}>
+                <IconButton
+                  sx={{
+                    backgroundColor: '#ff6341',
+                    color: '#fff',
+                    width: '64px',
+                    height: '64px',
+                    marginBottom: 2,
+                    '&:hover': {
+                      backgroundColor: '#ff8261',
+                    },
+                  }}
+                >
+                  <Groups />
+                </IconButton>
+                <CardContent>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      fontWeight: 'bold',
+                      color: '#ff6341',
+                    }}
+                  >
+                    For New Immigrants
+                  </Typography>
+                  <Typography variant="body2" sx={{ marginBottom: 2, color: '#fff' }}>
+                    Information and services for newcomers to New Brunswick.
+                  </Typography>
+                  <IconButton
+                    sx={{
+                      color: '#ff6341',
+                      transform: expandedImmigrant ? 'rotate(180deg)' : 'rotate(0deg)',
+                      transition: 'transform 0.3s ease-in-out',
+                      alignSelf: 'flex-end',
+                    }}
+                  >
+                    <ExpandMore />
+                  </IconButton>
+                </CardContent>
+              </CardActionArea>
+              <Collapse in={expandedImmigrant} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <List>
+                    {data.immigrantLinks.map((link, index) => (
+                      <ListItem key={index}>
+                        <ListItemText
+                          primary={`${index + 1}. ${link.title}`}
+                          secondary={link.description}
+                          primaryTypographyProps={{ style: { color: '#ff6341' } }}
+                          secondaryTypographyProps={{ style: { color: '#fff' } }}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
+                </CardContent>
+              </Collapse>
+            </Card>
+          </Zoom>
+        </Grid>
+
+        {/* General Links */}
+        {data.links.map((link, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
             <Zoom triggerOnce={false}>
               <Card
@@ -212,8 +311,8 @@ const UsefulLinks = () => {
                   borderRadius: '15px',
                   '&:hover': {
                     backgroundColor: '#112240',
-                    transform: 'scale(1.05)',
-                    transition: '0.3s',
+                    transform: 'translateY(-10px)',
+                    transition: '0.3s ease-in-out',
                   },
                 }}
               >
@@ -230,7 +329,7 @@ const UsefulLinks = () => {
                       },
                     }}
                   >
-                    {link.icon}
+                    {iconMapping[link.icon]}
                   </IconButton>
                   <CardContent>
                     <Typography
@@ -242,15 +341,18 @@ const UsefulLinks = () => {
                     >
                       {link.title}
                     </Typography>
-                    <Typography variant="body2">{link.description}</Typography>
+                    <Typography variant="body2" sx={{ color: '#fff' }}>
+                      {link.description}
+                    </Typography>
                   </CardContent>
                 </CardActionArea>
               </Card>
             </Zoom>
           </Grid>
         ))}
+
       </Grid>
-    </Box>
+    </Container>
   );
 };
 
